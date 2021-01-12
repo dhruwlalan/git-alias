b92ddd1d238635b3a605015a73fcf35415e112a9
dhruw <dhruwlalan22@gmail.com>
2021-01-13 00:08:45 +0530

    seperated alias into its own comp

--- src/components/cards/BasicCard.vue
+++ src/components/cards/BasicCard.vue
@@ -2,26 +2,19 @@
    <div class="main">
       <div class="main__header">BASIC</div>
       <div class="main__body">
-         <div v-for="alias in aliases" :key="alias.name" class="main__body__alias">
-            <div class="main__body__alias--title">{{ alias.title }}</div>
-            <div class="main__body__alias--syntax">
-               <span>$</span>
-               <span>&nbsp;</span>
-               <span>git</span>
-               <span>&nbsp;</span>
-               <span class="main__body__alias--syntax--name">{{ alias.name }}</span>
-               <span>&nbsp;</span>
-               <span>{{ alias.rest }}</span>
-            </div>
-         </div>
+         <the-alias v-for="alias in aliases" :key="alias.name" :alias="alias" />
       </div>
    </div>
 </template>
 
 <script>
 import { basic } from '../vanilla/alias';
+import TheAlias from './TheAlias.vue';
 
 export default {
+   components: {
+      TheAlias,
+   },
    computed: {
       aliases() {
          return basic;
---
+++ src/components/cards/TheAlias.vue
@@ -0,0 +1,47 @@
+<template>
+   <div class="main__body__alias" @mouseenter="hover = true" @mouseleave="hover = false">
+      <div class="main__body__alias--title">{{ alias.title }}</div>
+      <div class="main__body__alias--syntax">
+         <span>$</span>
+         <span>&nbsp;</span>
+         <span>git</span>
+         <span>&nbsp;</span>
+         <span class="main__body__alias--syntax--name">{{ alias.name }}</span>
+         <span>&nbsp;</span>
+         <span>{{ alias.rest }}</span>
+         <copy-svg
+            class="main__body__alias--syntax--copy-svg"
+            v-if="hover && !copy"
+            @click="copyCode"
+         />
+         <copied-svg v-if="hover && copy" class="main__body__alias--syntax--copied-svg" />
+      </div>
+   </div>
+</template>
+
+<script>
+import CopySvg from '../utils/CopySvg.vue';
+import CopiedSvg from '../utils/CopiedSvg.vue';
+
+export default {
+   components: {
+      CopySvg,
+      CopiedSvg,
+   },
+   props: ['alias'],
+   data() {
+      return {
+         hover: false,
+         copy: false,
+      };
+   },
+   methods: {
+      copyCode() {
+         this.copy = true;
+         setTimeout(() => {
+            this.copy = false;
+         }, 1000);
+      },
+   },
+};
+</script>
