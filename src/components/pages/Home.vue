e29b18e1e1b1377d0cbe6945179d300b6022fe32
dhruw <dhruwlalan22@gmail.com>
2021-03-08 17:02:16 +0530

    add links to home page

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -5,6 +5,30 @@
          <h1 class="home__header__title">Git Alias</h1>
       </div>
       <p class="home__description">Some simple aliases for commonly used git commands.</p>
-      <div class="home__links"></div>
+      <div class="home__links">
+         <a class="home__links--link" href="https://github.com/dhruwlalan/git-alias">
+            <github-svg class="home__links--link--logo" />
+            <p class="home__links--link--text">Github</p>
+         </a>
+         <a class="home__links--link" href="../../../git-config/.gitconfig" download=".gitconfig">
+            <download-svg class="home__links--link--logo" />
+            <p class="home__links--link--text">.gitconfig</p>
+         </a>
+         <div class="home__links--link" @click="$router.push({ name: 'basic' })">
+            View All Aliases
+         </div>
+      </div>
    </div>
 </template>
+
+<script>
+import GithubSvg from '../utils/GithubSvg.vue';
+import DownloadSvg from '../utils/DownloadSvg.vue';
+
+export default {
+   components: {
+      GithubSvg,
+      DownloadSvg,
+   },
+};
+</script>
---
+++ src/components/utils/DownloadSvg.vue
@@ -0,0 +1,35 @@
+<template>
+   <svg
+      width="24"
+      height="24"
+      xmlns="http://www.w3.org/2000/svg"
+      class="iconify iconify--majesticons"
+      preserveAspectRatio="xMidYMid meet"
+   >
+      <g>
+         <title>Layer 1</title>
+         <g stroke="null" fill="none" id="svg_1">
+            <path
+               stroke="null"
+               fill-rule="evenodd"
+               clip-rule="evenodd"
+               d="m3,19.34994a3,3.14997 0 0 0 3,3.14997l12,0a3,3.14997 0 0 0 3,-3.14997l0,-10.68051a3,3.14997 0 0 0 -0.879,-2.22598l-3.828,-4.02147a3,3.14997 0 0 0 -2.121,-0.92189l-8.172,0a3,3.14997 0 0 0 -3,3.14997l0,14.69987l0,0.00001zm10,-7.34994a1,1.04999 0 1 0 -2,0l0,3.76527l-1.293,-1.35763a1,1.04999 0 0 0 -1.414,1.48469l3,3.14997a1,1.04999 0 0 0 1.414,0l3,-3.14997a1,1.04999 0 0 0 -1.414,-1.48469l-1.293,1.35763l0,-3.76527z"
+               fill="currentColor"
+               id="svg_2"
+            />
+         </g>
+         <rect
+            stroke="null"
+            fill="#00ffff"
+            opacity="undefined"
+            x="10.87507"
+            y="7.28163"
+            width="2.18737"
+            height="10.68676"
+            id="svg_3"
+            stroke-width="0"
+            rx="1"
+         />
+      </g>
+   </svg>
+</template>
